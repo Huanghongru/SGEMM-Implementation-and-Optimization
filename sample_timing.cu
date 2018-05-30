2d7856b907280c7d7fd61d6acebf07c5639dcffb
huanghongru <huanghongru1997@gmail.com>
2018-05-30 20:21:06 +0800

    add sample timing

--- sample_timing.cu
+++ sample_timing.cu
@@ -4,9 +4,20 @@
 
 #define N 10
 
+/* CUDA offers a relatively light-weight alternative to
+     * CPU timers via the CUDA event API
+     * The logic of usage is as follows.
+     */
+
 int main() {
     int a[N], b[N], c[N];
     int *dev_a, *dev_b, *dev_c;
+    
+    // Create CUDA event obeject by cudaEvent_t
+    // Use cudaEventCreate API to create    
+    cudaEvent_t start, stop;
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
 
     cudaMalloc((void**)&dev_a, N*sizeof(int));
     cudaMalloc((void**)&dev_b, N*sizeof(int));
@@ -20,13 +31,24 @@ int main() {
     cudaMemcpy(dev_a, a, N*sizeof(int), cudaMemcpyHostToDevice);
     cudaMemcpy(dev_b, b, N*sizeof(int), cudaMemcpyHostToDevice);
 
+    // Squeeze the function to time by invoke API
+    // cudaEventRecord twice
+    cudaEventRecord(start);
     add<<<N, 1>>>(dev_a, dev_b, dev_c, N);
+    cudaEventRecord(stop);
 
     cudaMemcpy(c, dev_c, N*sizeof(int), cudaMemcpyDeviceToHost);
+    cudaEventSynchronize(stop);
 
     for (int i = 0; i < N; ++i) {
         printf("%d + %d = %d\n", a[i], b[i], c[i]); 
     }std::cout << std::endl;
+   
+    // Refer a float type variable to calculate
+    // the elapsed time. 
+    float t = 0;
+    cudaEventElapsedTime(&t, start, stop);
+    printf("add takes %f ms to complete\n", t);
 
     cudaFree(dev_a);
     cudaFree(dev_b);
