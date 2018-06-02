32c25e54cd1e6383034a7968f2c259820ae24bba
huanghongru <huanghongru1997@gmail.com>
2018-06-02 17:12:11 +0800

    add random generation function for GPU allocation

--- utils.cpp
+++ utils.cpp
@@ -29,12 +29,61 @@ T** random_fill_matrix(int row, int col, T min=0, T max=100) {
 }
 
 template <typename T>
+T* random_matrix_gpu(int row, int col, T min=0, T max=100) {
+    /* A function to quickly generate a matrix in some range [min, max)
+     * Note that it is very hard to allocate 2-d array on GPU,
+     * so in most of the cases, we pass the 2-d array as a 1-d array
+     * to the device following row-major or column-major order.
+     *
+     * Parameters:
+     * ----------
+     *   row: number of rows of matrix
+     *   col: number of columns of matrix
+     *   min, max: the range of random number. default to [0, 100)
+     *
+     * Returns:
+     * -------
+     *    a specific type 1d pinter pointed to the matrix
+     */
+    T* mat = new T[row*col];
+    std::random_device rd;
+    std::mt19937 mt(rd());
+    std::uniform_real_distribution<T> unif(min, max);
+    for (int i = 0; i < row; ++i) {
+	for (int j = 0; j < col; ++j) {
+	    mat[i*row+j] = unif(mt);
+	}
+    }
+    return mat;
+}
+
+template <typename T>
+void print_mat_gpu(T* mat, int row, int col) {
+    for (int i = 0; i < row; ++i) {
+	for (int j = 0; j < col; ++j) {
+	    std::cout << mat[i*row + j] << " ";
+	}std::cout << std::endl;
+    }
+}
+
+template <typename T>
 void print_mat(T** mat, int row, int col) {
     // Display the matrix for visualizatoin
     for (int i = 0; i < row; ++i) {
         for (int j = 0; j < col; ++j) {
             std::cout << mat[i][j] << " ";
         }std::cout << std::endl;
+    }
+}
+
+template <typename T>
+void check_sum(T** a, T** b, T** c, int row, int col) {
+    for (int i = 0; i < row; ++i) {
+        for (int j = 0; j < col; ++j) {
+            std::cout << a[i][j] << " + " << b[i][j]
+		    << " = " << c[i][j] << " "
+		    << bool(a[i][j]+b[i][j]==c[i][j]) << std::endl;
+	}
     }
 }
     
