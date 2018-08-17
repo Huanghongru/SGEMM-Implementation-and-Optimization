5b261445bd5b953c67121d02531be9e794c27bdf
huanghongru <huanghongru1997@gmail.com>
2018-08-17 12:28:35 -0400

    add corder and fotran order features

--- utils.cpp
+++ utils.cpp
@@ -3,6 +3,10 @@
 #include <random>
 #include <cmath>
 
+namespace utils {
+int C_ORDER = 1;
+int FORTRAN_ORDER = 2;
+
 template <typename T>
 T** random_fill_matrix(int row, int col, T min=0, T max=100) {
     /* A function to quickly generate a matrix in some range [min, max]
@@ -30,7 +34,7 @@ T** random_fill_matrix(int row, int col, T min=0, T max=100) {
 }
 
 template <typename T>
-T* random_matrix_gpu(int row, int col, T min=0, T max=100) {
+T* random_matrix_gpu(int row, int col, int order_type, T min=-50, T max=50) {
     /* A function to quickly generate a matrix in some range [min, max)
      * Note that it is very hard to allocate 2-d array on GPU,
      * so in most of the cases, we pass the 2-d array as a 1-d array
@@ -40,7 +44,7 @@ T* random_matrix_gpu(int row, int col, T min=0, T max=100) {
      * ----------
      *   row: number of rows of matrix
      *   col: number of columns of matrix
-     *   min, max: the range of random number. default to [0, 100)
+     *   min, max: the range of random number. default to [-50, 50)
      *
      * Returns:
      * -------
@@ -50,20 +54,36 @@ T* random_matrix_gpu(int row, int col, T min=0, T max=100) {
     std::random_device rd;
     std::mt19937 mt(rd());
     std::uniform_real_distribution<T> unif(min, max);
-    for (int i = 0; i < row; ++i) {
-	for (int j = 0; j < col; ++j) {
-	    mat[i*row+j] = unif(mt);
-	}
+    if (order_type == C_ORDER ) {
+        for (int i = 0; i < row; ++i) {
+	    for (int j = 0; j < col; ++j) {
+	        mat[i*row+j] = unif(mt);
+	    }
+        }
+    } else {
+        for (int i = 0; i < row; ++i) {
+	    for (int j = 0; j < col; ++j) {
+	        mat[i+j*row] = unif(mt);
+	    }
+        }
     }
     return mat;
 }
 
 template <typename T>
-void print_mat_gpu(T* mat, int row, int col) {
-    for (int i = 0; i < row; ++i) {
-	for (int j = 0; j < col; ++j) {
-	    std::cout << mat[i*row + j] << " ";
-	}std::cout << std::endl;
+void print_mat_gpu(T* mat, int row, int col, int order_type) {
+    if (order_type == C_ORDER) {
+        for (int i = 0; i < row; ++i) {
+	    for (int j = 0; j < col; ++j) {
+	        std::cout << mat[i*row + j] << " ";
+	    }std::cout << std::endl;
+        }
+    } else {
+        for (int i = 0; i < row; ++i) {
+	    for (int j = 0; j < col; ++j) {
+	        std::cout << mat[i + j*row] << " ";
+	    }std::cout << std::endl;
+        }
     }
 }
 
@@ -97,28 +117,44 @@ void check_sum(T* a, T* b, T* c, int row, int col) {
 }
 
 template <typename T>
-bool check_mul(T* a, T* b, T* c, int M, int K, int N) {
+bool check_mul(T* a, T* b, T* c, int M, int K, int N, int order_type) {
     /* Check if the result of matrix multiplication is right.*/
-    for (int i = 0; i < M; ++i) {
-	for (int j = 0; j < N; ++j) {
-	    T value = 0;
-	    for (int k = 0; k < K; ++k) {
-		value += a[i*M+k]*b[k*K+j];
-            }
-	    if (fabs(value-c[i*M+j])>1e-5) {
-		std::cout << c[i*M+j] << " " << value << std::endl;
-		return false;
+    if (order_type == C_ORDER) {
+	for (int i = 0; i < M; ++i) {
+	    for (int j = 0; j < N; ++j) {
+		T value = 0;
+		for (int k = 0; k < K; ++k) {
+		    value += a[i*M+k]*b[k*K+j];
+		}
+		if (fabs(value-c[i*M+j])>1e-5) {
+		    std::cout << c[i*M+j] << " " << value << std::endl;
+		    return false;
+		}
 	    }
-        }
+	}
+    } else {
+	for (int i = 0; i < M; ++i) {
+            for (int j = 0; j < N; ++j) {
+		T value = 0;
+		for (int k = 0; k < K; ++k) {
+		    value += a[i+k*M]*b[k+j*K];
+		}
+		if (fabs(value-c[i+j*M])>1e-5) {
+		    std::cout << c[i+j*M] << " " << value << std::endl;
+		    return false;
+		}
+	    }
+	}
     }
     return true;
+}
 }
 /*
 int main() {
     int a[4] = {1, 2, 3, 4};
     int b[4] = {1, 2, 3, 4};
     int c[4] = {6, 10, 15, 22};
-    std::cout << check_mul<int>(a, b, c, 2, 2, 2) << std::endl;
+    std::cout << utils::check_mul<int>(a, b, c, 2, 2, 2) << std::endl;
     std::cout << fabs(-1.34) << std::endl;
     return 0;
 }*/
