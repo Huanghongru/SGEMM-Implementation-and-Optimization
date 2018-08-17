f720fa0ca0f38bfd3becaa4a711c6cd0aa1ebc6e
huanghongru <huanghongru1997@gmail.com>
2018-08-17 12:27:53 -0400

    fix bug

--- laboratory.py
+++ laboratory.py
@@ -24,11 +24,10 @@ def main():
             t = l[4].split()[3]
             if re.compile(r'(\d+)(\.|)(\d+)us').match(t):
                 t = eval(t[:-2])*1e-6
-            if re.compile(r'(\d+)(\.|)(\d+)ms').match(t):
-                print t
+            elif re.compile(r'(\d+)(\.|)(\d+)ms').match(t):
                 t = eval(t[:-2])*1e-3
-            # if re.compile(r'(\d+)(\.|)(\d+)s').match(t):
-            #     t = eval(t[:-1])
+            elif re.compile(r'(\d+)(\.|)(\d+)s').match(t):
+                t = eval(t[:-1])
             print "Matrix size: %s\tKernel performance: %.2f GFlop/s" % (ms, gflops(m, k, n, t))
 
 if __name__ == '__main__':
